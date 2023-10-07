92a6e004efb8cd0344841446df68e3e00ea190d7
Angga Saputra <angsa0077@gmail.com>
2023-10-07 08:59:13 +0700

    install dan inisialisasi token login

---
+++ react/src/components/DefaultLayout.jsx
@@ -0,0 +1,17 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
+
+const DefaultLayout = () => {
+    const { user, token } = useStateContext();
+
+    if (!token) {
+        return <Navigate to="/login" />
+    }
+    return (
+        <div>
+            <Outlet />
+        </div>
+    )
+}
+
+export default DefaultLayout
---
+++ react/src/views/Signup.jsx
@@ -0,0 +1,11 @@
+
+
+const Signup = () => {
+    return (
+        <div>
+            Singup
+        </div>
+    )
+}
+
+export default Signup
